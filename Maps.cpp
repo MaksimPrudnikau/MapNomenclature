4ea34249b9644cd7dc548e91596fe39dff32cdde
Channel BelarusGame <Mrfreedok@gmail.com>
2020-09-04 08:57:50 +0300

    Определение первой буквы карты

--- Maps.cpp
+++ Maps.cpp
@@ -4,29 +4,44 @@
 
 Map::Map()
 {
-	Angle angle = { 0,0 };
+	x_angle = y_angle = { 0,0 };
 	x = 0;
 	y = 0;
 }
 
 void Map::importCoordinates()
 {
-	cout << "¬ведите координаты" << endl;
+	/*cout << "¬ведите координаты" << endl;
 	cout << "X: ";
 	cin >> x;
 	cout << "Y: ";
-	cin >> y;
+	cin >> y;*/
+	x = 54.002507;
+	y = 27.951434;
 }
 
 void Map::transferGradToGradMin()
 {
-	x_angle.grad = trunc(x);
+	x_angle.grad = trunc(abs(x));
 	x_angle.min = (x - x_angle.grad) * 60;
 	x_angle.sec = (((x - x_angle.grad) * 60) - x_angle.min) * 60;
 
-	y_angle.grad = trunc(y);
+	y_angle.grad = trunc(abs(y));
 	y_angle.min = (y - y_angle.grad) * 60;
 	y_angle.sec = (((y - y_angle.grad) * 60) - y_angle.min) * 60;
+}
+
+void Map::set_1Letter()
+{
+	int number = trunc((x_angle.grad + 4) / 4);
+	card.Letter = TimeZone[number - 1];
+	latitude_remainder.grad = x_angle.grad - (number * 4 - 4);
+	latitude_remainder.min = x_angle.min;
+}
+
+void Map::set_1value()
+{
+
 }
 
 void Map::showAngles()
@@ -38,4 +53,5 @@ void Map::showAngles()
 void Map::calculate()
 {
 	transferGradToGradMin();
-}
+	set_1Letter();
+} 
--- Maps.h
+++ Maps.h
@@ -1,4 +1,7 @@
 #pragma once
+
+const static char TimeZone[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'Y', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V' };
+
 class Map
 {
 	struct Angle
@@ -6,9 +9,22 @@ class Map
 		int grad;
 		int min;
 		int sec;
-	}x_angle, y_angle;
+	}x_angle, y_angle, latitude_remainder, longtitude_remainder;
 
 	double x, y;
+	
+	struct Card
+	{
+		char Letter;
+		int
+			_1value,
+			_2value;
+		char
+			_2Letter,
+			_3letter;
+		int 
+			_3value;
+	}card;
 
 public:
 	Map();
@@ -17,5 +33,7 @@ public:
 	void calculate();
 private:
 	void transferGradToGradMin();
+	void set_1Letter();
+	void set_1value();
 };
 
